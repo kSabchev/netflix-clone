188f9f8fbe58c488cb1fd1e002ba04bbc4d9ddd2
kSabchev <kaloyansabchev@gmail.com>
2021-02-05 01:20:38 +0200

    finished day 3

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,27 +1,52 @@
-import React from "react";
-import "./App.css";
-import HomeScreen from "./screens/HomeScreen";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import LoginScreen from "./screens/LoginScreen";
+import React, { useEffect } from 'react'
+import './App.css'
+import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import LoginScreen from './screens/LoginScreen'
+import { auth } from './firebase'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
+import ProfileScreen from './screens/ProfileScreen'
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+
+    return unsubscribe
+  }, [dispatch])
 
   return (
-    <div className="app">
+    <div className='app'>
       <Router>
-        {!user? (
+        {!user ? (
           <LoginScreen />
         ) : (
           <Switch>
-            <Route path="/">
+            <Route path='/profile'>
+              <ProfileScreen />
+            </Route>
+            <Route exact path='/'>
               <HomeScreen />
             </Route>
           </Switch>
         )}
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- netflix-clone/src/firebase.js
+++ netflix-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDZscdgWrDpGSGqVGQM_725-kwE7VPkg9I",
     authDomain: "netflix-clone-decff.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "200352291646",
     appId: "1:200352291646:web:f3921d1618b6b9aef96a6f"
   };
+  
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth }
+export default db
